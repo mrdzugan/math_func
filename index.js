ab3906892db360512e1f7d3f208867cf2af51b3b
Andrei Dzugan <mr.dzugan@gmail.com>
2020-07-07 00:44:25 +0300

    Finish work with Math.pow(), Math.sqrt()
    - Reformat: Added task numbers

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 // ========= WORK WITH %
 
-console.log(remainderOfDivision(32, 6));
-
+// #1
 function remainderOfDivision(a, b) {
     return a % b;
 }
 
+// #2
 function remainderOfDivisionAdvanced(a, b) {
     return a % b === 0 ? 'Делится' : `Делится с остатком ${a % b}`;
+}
+
+// ========= WORK WITH Math.pow(), Math.sqrt()
+
+// #3
+const st = Math.pow(2, 10);
+
+// #4
+function sqrt245() {
+    return Math.sqrt(245);
+}
+
+// #5
+function sqrtFromArray() {
+    const arr = [4, 2, 5, 19, 13, 0, 10];
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += Math.pow(arr[i], 3);
+    }
+    return Math.sqrt(sum);
 }
