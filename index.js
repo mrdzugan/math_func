3cdaab40f113571bba0493d41fdab803ee65ece9
Andrei Dzugan <mr.dzugan@gmail.com>
2020-07-07 00:34:39 +0300

    Finish work with % operator
    - Add advanced remainder of the division function

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ console.log(remainderOfDivision(32, 6));
 function remainderOfDivision(a, b) {
     return a % b;
 }
+
+function remainderOfDivisionAdvanced(a, b) {
+    return a % b === 0 ? 'Делится' : `Делится с остатком ${a % b}`;
+}
